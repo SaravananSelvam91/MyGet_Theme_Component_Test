42a5d93b0b654c9b51da7c498e4bd0c62f5aae02
SaravananSelvam91 <saravanan.selvam@delta.com>
2016-10-12 16:15:38 +0530

    Test Repository
    TestCode

---
+++ DL.UITheme/Styles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DL.UITheme
+{
+	public partial class Styles
+	{
+		public Styles()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
